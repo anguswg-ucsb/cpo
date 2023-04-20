cf4fb3f69276cc0447e6e26539509d70e653c248
YOUR NAME <Your Email Here>
2023-04-20 14:20:33 -0700

    regression plots, timeseries plot cleanups, correlation plots

--- R/explore.R
+++ R/explore.R
@@ -9,16 +9,17 @@ library(ggplot2)
 library(sf)
 library(corrplot)
 library(patchwork)
+library(GGally)
+library(ggpubr)
 
 # Pull in climate/call analysis data for modelling.
 # CAUTION, if climate and call analysis data is not already pulled and saved to data/ folder, then this operation can take a while as all climate data and call analysis data must be pulled and downloaded from the internet
 source("R/get_model_data.R")
 source("R/plot_utils.R")
 
-
-# ****************************
+# ----------------------------
 # ---- Group by seniority ----
-# ****************************
+# ----------------------------
 
 # local path to save plots to
 save_path <- "D:/cpo/plots"
@@ -26,21 +27,45 @@ save_path <- "D:/cpo/plots"
 # loop through all climate variables and make a faceted district scatter plots for each climate variable vs out priority %
 make_out_scatter_plots(df = mod_df, save_path = save_path)
 
-# *************************************
+# -------------------------------------
 # ---- Dive into a single district ----
-# *************************************
-library(GGally)
-doi <- c("02")
-start_date = "2000-01-01"
-end_date   = "2002-01-01"
+# -------------------------------------
 
-# dataframe names
-names(mod_df)
+# district of interest
+doi <- c("02")
 
 # make a subset of the data
 sub_df <-
   mod_df %>%
-  dplyr::filter(district %in% c(doi), date > start_date, date < end_date)
+  dplyr::filter(district %in% c(doi), date > start_date, date < end_date) %>%
+  # dplyr::filter(out_pct > 0) %>%
+  dplyr::mutate(
+    out_pct_sqrt = sqrt(out_pct)
+  )
+# --------------------------
+# ---- Correlation plot ----
+# --------------------------
+# Correlation between climate variables
+corr_matrix <- cor(dplyr::select(sub_df, -district, -date, -wdid , -gnis_id, -approp_date, -seniority), use = "pairwise.complete.obs")
+
+# Create a correlation plot
+corrplot(corr_matrix, method = "circle")
+
+# -------------------------
+# ---- Regression plot ----
+# -------------------------
+# create scatter regression plots
+ggscatter(sub_df,
+          x = "out_pct_sqrt",
+          y = "eddi1y",
+          add = "reg.line", conf.int = TRUE,
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "X Label", ylab = "Y Label",
+          title = "Correlation Plot")
+
+# --------------------------
+# ---- Timeseries plots ----
+# --------------------------
 
 # plot a district out of priority percent facet plot, plot columns are GNIS IDs and rows are the
 # relative seniority on that GNIS ID (junior, median, senior)
@@ -48,12 +73,11 @@ sub_df %>%
   ggplot2::ggplot() +
   ggplot2::geom_line(ggplot2::aes(x = date, y = out_pct)) +
   ggplot2::facet_grid(seniority~gnis_id)
-gnis_id = "180770"
+
 
 sub_clim <-
   mod_df %>%
   dplyr::filter(district %in% c(doi)) %>%
-  # dplyr::filter(gnis_id == "180770") %>%
   dplyr::select(date, wdid, gnis_id, seniority, out_pct, pr, tmmx, spi30d, eddi30d, eddi90d, pdsi)
 
 # climate variable vs out_pct
@@ -82,53 +106,3 @@ sub_clim %>%
 
 # ************************************************************************
 
-library(ggplot2)
-library(tidyr)
-library(GGally)
-
-# calculate mean and standard deviation across districts and water rights
-aggreg_df <-
-    mod_df %>%
-    # dplyr::filter(district %in% c("02", "07")) %>%
-    # dplyr::filter(district %in% c("02")) %>%
-    dplyr::group_by(date, district, seniority) %>%
-    dplyr::summarise(
-        dplyr::across(c(out_pct:z), list(mean = ~ mean(., na.rm = TRUE), sd = ~ sd(., na.rm = TRUE)))
-        )
-
-aggreg_df2 <-
-  aggreg_df %>%
-  dplyr::select(date, district, seniority, contains("mean")) %>%
-  dplyr::filter(district %in%  c("06", "01", "07", "05", "64", "08", "09")) %>%
-  dplyr::mutate(
-    year = lubridate::year(date)
-  )
-
-aggreg_df2 %>%
-  dplyr::filter(year %in% c(2003)) %>%
-  ggplot2::ggplot() +
-  ggplot2::geom_line(ggplot2::aes(x = date, y = out_pct_mean)) +
-  ggplot2::facet_grid(seniority~district)
-
-# ---- Correlation plot ----
-# Correlation between climate variables
-corr_matrix <- cor(dplyr::select(clim_ts, -district, -date), use = "pairwise.complete.obs")
-
-# Create a correlation plot
-corrplot(corr_matrix, method = "circle")
-
-# mod_df <-
-#     call_df %>%
-#     dplyr::left_join(
-#         clim_ts,
-#         by = c("district", "date")
-#     )
-#
-# aggreg_df <-
-#     mod_df %>%
-#     # dplyr::filter(district %in% c("02", "07")) %>%
-#     dplyr::filter(district %in% c("02")) %>%
-#     dplyr::group_by(date, district, seniority) %>%
-#     dplyr::summarise(
-#         dplyr::across(c(out_pct:z), list(mean = ~ mean(., na.rm = TRUE), sd = ~ sd(., na.rm = TRUE)))
-#         )
