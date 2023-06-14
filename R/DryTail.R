6a0bd9ffbe0053e8d40f78c5ca5d71cb572466b9
Emma Golub <emmaa.golub@gmail.com>
2023-06-14 15:07:18 -0700

    drytail lower half and terciles, still filtering call_model based on those years

--- R/DryTail.R
+++ R/DryTail.R
@@ -2,43 +2,36 @@
 # Emma Golub
 # 6/7/23
 
-#install.packages("cdssr")
+# Install.packages("cdssr")
 library(cdssr)
 library(dplyr)
 library(tidyverse)
 library(magrittr)
 
-# load in utils.R functions
+output_data_dir <- "./data"
+
+# Load in utils.R functions
 source("R/utils.R")
 
-# path to reference table
+# Path to reference table
 ref_tbl_path <- "data/district_lookup_table.csv"
-
-# reference table
-#ref_tbl <- readr::read_csv(ref_tbl_path)
 ref_tbl <- read.csv(ref_tbl_path)
-# Remove the 3rd through 6th columns
 ref_tbl <- ref_tbl[, -c(3:6)]
-# Remove rows after the 16th row
 ref_tbl <- ref_tbl[1:16, ]
 
-##### Load in observed streamflow (cfs) from CDSSR #####
-print(seq_len(nrow(ref_tbl)))
-
+# Load in observed streamflow (cfs) from CDSSR
 streamflow_df <- lapply(1:nrow(ref_tbl), function(d) { # for each district (row) in the table
 
   logger::log_info("Getting district {ref_tbl[d, ]$district} streamflow data...")
 
-
   ids <- sapply(tidyr::separate_rows(ref_tbl[d, ], usgs_id, sep = ", ")$usgs_id, as.character)
-  #lapply over every row in reg_tbl and separate attributes in column usgs_id
 
   streamflow_list <- lapply(ids, function(id) { # for each id in the ids list
     if (!grepl("^0", id)) {
       id <- paste0("0", id)
     } else if (TRUE) {
-      # Continue loop or perform specific operations
     }
+
     streamflow <- cdssr::get_sw_ts(
       usgs_id = id,
       start_date = "1970-01-01",
@@ -47,6 +40,7 @@ streamflow_df <- lapply(1:nrow(ref_tbl), function(d) { # for each district (row)
     )
     return(streamflow)
   })  %>%
+
   bind_rows() %>%
     mutate(district = ref_tbl[d, ]$district)
 
@@ -59,22 +53,29 @@ flow_df <- streamflow_df %>%
   select(-station_num, -abbrev, -meas_type, -meas_count, -data_source, -modified) %>%
   rename(year = water_year, usgs_id = usgs_site_id)
 
-# Expand the list of usgs site ids in each district into their own rows
+# Build crosswalk and make sure id's all have "0" as first character to avoid double-counting the same IDs
 crosswalk <- ref_tbl %>%
-  separate_rows(usgs_id, sep = ",") # this correctly has all districts
+ separate_rows(usgs_id, sep = ", ")
+
+for (id in 1:nrow(crosswalk)) {
+  usgs_id <- crosswalk$usgs_id[id] # Get the value of "usgs_id" for the current row
+
+  if (!grepl("^0", usgs_id)) {
+    crosswalk$usgs_id[id] <- paste0("0", usgs_id) # Add "0" as the first character if it's missing
+  }
+}
+
+# Yay! Now crosswalk and flow_df both have 8 unique usgs_ids.
 
 # Initialize an empty list to store the final dataframes
 df_list <- list()
 
 # Loop through each unique district in flow_df
 for (i in 1:length(unique(flow_df$district))) { # for i in unique districts
-  # Filter the crosswalk based on the current district
+
   condition <- crosswalk$usgs_id[crosswalk$district == unique(flow_df$district)[i]] # watch the parentheses here!
 
-  # Filter the flow_df based on the condition
-  # Here will be some usgs_ids that appear multiple times in the crosswalk because
-  #  they are relevant for multiple districts. Because of this, we need to make sure to
-  #  filter the flow_df by both the condition vector AND the relevant district in the loop
+  # filter the flow_df by both the condition vector AND the relevant district in the loop
   temp <- flow_df %>%
     filter(usgs_id %in% condition,
       district == unique(flow_df$district)[i]) %>%
@@ -88,51 +89,91 @@ for (i in 1:length(unique(flow_df$district))) { # for i in unique districts
 # Combine all dataframes in the list into one dataframe
 final_df <- bind_rows(df_list)
 
-# something is still not right with this final df... missing districts!
+# Yay! All the districts are there now. This just yields a weird "cachekey" error, but it doesn't affect the final dataframe
 
 
 ################################################################################
-# Next, find the dry tail years.
-# From average flow data in each year by district, determine which years are DRY (i.e. the driest 50% years, get a list of years for each district that will be half the number of years in each district)
+# Find dry tail years.
+################################################################################
 
-  # Determine the years with the bottom 50% driest streamflow for each district
-  # result <- aggregate(tot_qaf ~ district, data = final_df, FUN = function(x) {
-  #   years <- final_df$year[x <= median(x)]
-  #   paste(years, collapse = ", ")
-  # })
+# Initialize an empty list to store the results
+drytail_lower_half <- list()
 
-# Calculate the median streamflow for each district
-median_streamflow <- aggregate(tot_qaf ~ district, data = final_df, FUN = median)
+# Loop through each unique district
+for (d in 1:length(unique(final_df$district))) {
+  # Calculate the median tot_qaf value for the current district
+  median_value <- median(final_df$tot_qaf[final_df$district == unique(final_df$district)[d]])
 
-# Create an empty dataframe to store the results
-result_df <- data.frame(district = character(), year = I(list()), stringsAsFactors = FALSE)
+  # Filter the final_df based on the condition and district
+  condition <- final_df$year[final_df$tot_qaf < median_value & final_df$district == unique(final_df$district)[d]]
 
-# Iterate over each district
-for (i in 1:nrow(median_streamflow)) {
-  # Get the current district
-  d <- median_streamflow$district[i]
-
-  # Get the median streamflow for the current district
-  med_qaf <- median_streamflow$tot_qaf[i]
-
-  # Subset the dataframe for the current district
-  district_data <- subset(final_df, district == d)
-
-  # Subset the dataframe to include only the years where streamflow is below the median
-  below_median <- subset(district_data, tot_qaf < med_qaf)
-
-  # Get the unique years below the median
-  unique_years <- unique(below_median$year)
-
-  # Append the unique years to the result dataframe
-  result_df <- rbind(result_df, data.frame(district = d, year = I(list(unique_years))))
+  # Filter the final_df by both the condition vector and the relevant district in the loop
+  drytail_lower_half[[d]] <- final_df[final_df$year %in% condition & final_df$district == unique(final_df$district)[d], ]
 }
 
-write_csv(result_df, "drytailyears_bydistrict.csv")
-
-
-
+# Combine all dataframes in the list into one dataframe
+drytail_lower_half <- do.call(rbind, drytail_lower_half)
+write_csv(drytail_lower_half, "drytail_lower_half.csv")
 
 
 # Determine the years with the bottom tercile driest streamflow for each district
+# Initialize an empty list to store the results
+drytail_lower_tercile <- list()
 
+# Loop through each unique district
+for (d in 1:length(unique(final_df$district))) {
+  # Calculate the tercile tot_qaf value for the current district
+  tercile_value <- quantile(final_df$tot_qaf[final_df$district == unique(final_df$district)[d]], 0.33)
+
+  # Filter the final_df based on the condition and district
+  condition <- final_df$year[final_df$tot_qaf < tercile_value & final_df$district == unique(final_df$district)[d]]
+
+  # Filter the final_df by both the condition vector and the relevant district in the loop
+  drytail_lower_tercile[[d]] <- final_df[final_df$year %in% condition & final_df$district == unique(final_df$district)[d], ]
+}
+
+# Combine all dataframes in the list into one dataframe
+drytail_lower_tercile <- do.call(rbind, drytail_lower_tercile)
+write_csv(drytail_lower_tercile, "drytail_lower_tercile.csv")
+
+
+
+###############################################################################
+# Plotting for Dry Tail Years
+###############################################################################
+
+# Filter dry tail years for each district in annual model data and save as a new csv.
+# Then call it in as the new df to run plots from Exploratory.R
+call_model <- read.csv("./data/annual_model_data.csv")
+
+#drytail_call_model <- call_model[call_model$year %in% drytail_lower_half$year & call_model$district %in% drytail_lower_half$district, ]
+
+# Initialize an empty list to store the filtered dataframes
+filtered_dfs <- list()
+
+# Loop through each unique value in district of call_model
+for (value1 in 1:length(unique(call_model$district))) {
+  # Loop through each unique value in district of drytail_lower_half
+  for (value2 in 1:length(unique(drytail_lower_half$district))) {
+    # Check if the values in column1 of both dataframes match
+    if (value1 == value2) {
+      # Filter call_model based on the current value of district
+      filtered_df <- call_model[call_model$district == unique(call_model$district) &
+                                  call_model$year %in% drytail_lower_half$year, ]
+      # Add the filtered dataframe to the list
+      filtered_dfs[[length(filtered_dfs) + 1]] <- filtered_df
+      # Break the inner loop to move to the next value in dataframe1
+      break
+    }
+  }
+}
+
+# Combine all filtered dataframes in the list into one dataframe
+result_df <- do.call(rbind, filtered_dfs)
+
+# RIGHT NOW, YEAR ORDER IS OFF, AND I THINK SOME YEARS ARE MISSING. BUT ALL DISTRICTS ARE THERE.
+
+
+#df <-
+
+#source("R/Exploratory.R")
