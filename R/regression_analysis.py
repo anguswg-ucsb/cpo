3298914215e633fd1c56b20c137a38f5690dc4b3
Emma Golub <emmaa.golub@gmail.com>
2023-07-17 13:07:31 -0700

    adjust NA filtering approach, improved performance updates v1

--- R/regression_analysis.py
+++ R/regression_analysis.py
@@ -11,9 +11,12 @@ import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
+#from sklearn import preprocessing, svm
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
 import numpy as np
+import scipy.stats as stats
 
 # Create an output directory
 output_dir = '../output/univariate_reg'
@@ -24,10 +27,19 @@ df = pd.read_csv("../data/annual_model_upd_eddi.csv")
 df = df.drop(columns=["basin", "water_source", "gnis_id", "approp_date"])
 
 # Wrangle data, exclude NAs
-# Exclude rows with missing values or insignificant district
-df = df.dropna()
 districts_to_exclude = [49, 65, 76]
 df = df[~df['district'].isin(districts_to_exclude)]
+# Check value types in the dataframe
+# print(df.dtypes) # they're all int64 or float64...
+
+# # Identify and remove outliers: find Q1, Q3, and interquartile range for each column
+# Q1 = df.quantile(q=.25) #BUG WORKING ON DATA TYPE ISSUE
+# Q3 = df.quantile(q=.75)
+# IQR = df.apply(stats.iqr)
+
+# # Filter to rows in dataframe that have values within 1.5*IQR of Q1 and Q3
+# df = df[~((df < (Q1-1.5*IQR)) | (df > (Q3+1.5*IQR))).any(axis=1)]
+# df.shape
 
 # Define key-value predictor-response pairings for each district
 district_variables = {
@@ -57,53 +69,76 @@ for district, (predictor_col, response_col) in district_variables.items():
     # Filter the DataFrame for the current district
     district_data = df[df['district'] == int(district)]
 
-    # Extract the predictor and response variables
+    # Exclude rows with missing values in the predictor and response columns
+    district_data = district_data.dropna(subset=[predictor_col, response_col])
+    print(district_data.shape)
+
+    # Extract the predictor and response vectors
     predictor = district_data[predictor_col]
     response = district_data[response_col]
 
-    # Perform linear regression
+# # Plot regression line and scatter plot to check relationships
+    # plt.figure(figsize=(6.5, 4.5), dpi=250)
+    # sns.regplot(x=predictor, y=response, ci=None, color='darkblue')
+    # plt.title(f'Linear Regression - District {district}', color='black')
+    # plt.xlabel(predictor_col, color='gray')
+    # plt.ylabel(response_col, color='gray')
+    # plt.xticks(color='gray')
+    # plt.yticks(color='gray')
+    # # Show summary statistics in the plot
+    # plt.text(0.05, 0.9, f'R-squared: {r2:.4f}', color='black', transform=plt.gca().transAxes)
+    # plt.text(0.05, 0.85, f'RMSE: {rmse:.4f}', color='black', transform=plt.gca().transAxes)
+    # plt.show()
+    # # Save the plot
+    # filename = os.path.join(output_dir, f'regression_district_{district}.png')
+    # plt.savefig(filename, dpi=250)
+
+    # Perform linear regression. Split data into training and testing.
+    X_train, X_test, y_train, y_test = train_test_split(predictor, response, test_size=0.25)
     model = LinearRegression()
-    model.fit(predictor.values.reshape(-1, 1), response)
-    predicted_response = model.predict(predictor.values.reshape(-1, 1))
+    model.fit(X_train.values.reshape(-1, 1), y_train)
+    y_pred = model.predict(X_test.values.reshape(-1, 1))
 
-    # Calculate R-squared
-    r2 = r2_score(response, predicted_response)
-
-    # Calculate RMSE
-    rmse = np.sqrt(mean_squared_error(response, predicted_response))
+    # Calculate metrics
+    r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    print("R2:", r2)
+    print("MAE:", mae)
+    print("MSE:", mse)
+    print("RMSE:", rmse)
 
     # Store regression results and summary statistics
-    regression_results[district] = (predictor, response, predicted_response)
-    summary_statistics[district] = {'R-squared': r2, 'RMSE': rmse}
+    regression_results[district] = (X_test, y_test, y_pred)
+    summary_statistics[district] = {'R-squared': r2, 'RMSE': rmse, 'MAE': mae, 'MSE': mse}
 
-    # Plot regression line and scatter plot
+    # Plot predicted vs. actual response
     plt.figure(figsize=(6.5, 4.5), dpi=250)
-    sns.regplot(x=predictor, y=response, ci=None, color='darkblue')
-    plt.title(f'Linear Regression - District {district}', color='black')
-    plt.xlabel(predictor_col, color='gray')
-    plt.ylabel(response_col, color='gray')
+    sns.regplot(x=y_pred, y=y_test, ci=None, color='darkblue')
+    plt.title(f'Univariate Linear Regression Performance - District {district}', color='black')
+    plt.xlabel('Predicted Response', color='gray')
+    plt.ylabel('Actual Response', color='gray')
     plt.xticks(color='gray')
     plt.yticks(color='gray')
-
     # Show summary statistics in the plot
     plt.text(0.05, 0.9, f'R-squared: {r2:.4f}', color='black', transform=plt.gca().transAxes)
     plt.text(0.05, 0.85, f'RMSE: {rmse:.4f}', color='black', transform=plt.gca().transAxes)
-
-    # Show the plot
     plt.show()
-
     # Save the plot
-    filename = os.path.join(output_dir, f'district_{district}.png')
-    plt.savefig(filename, dpi=250)
+    filename = os.path.join(output_dir, f'performance_district_{district}.png')
+    plt.savefig(filename, dpi=250) # why is it printing out blank
+
 
 # Display summary statistics for each district
 for district, stats in summary_statistics.items():
     print(f"District {district}:")
     print(f"R-squared: {stats['R-squared']:.4f}")
     print(f"RMSE: {stats['RMSE']:.4f}")
+    print(f"MSE: {stats['MSE']:.4f}")
+    print(f"MAE: {stats['MAE']:.4f}")
     print()
     
-
 
 #######################################################################
 # Multiple Linear Regression (run this separately from the first)
@@ -115,9 +150,13 @@ import pandas as pd
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
+#from sklearn import preprocessing, svm
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 import numpy as np
+import scipy.stats as stats
 
 # Create an output directory
 output_dir = '../output/multiple_reg'
@@ -128,26 +167,24 @@ df = pd.read_csv("../data/annual_model_upd_eddi.csv")
 df = df.drop(columns=["basin", "water_source", "gnis_id", "approp_date"])
 
 # Wrangle data, exclude NAs
-# Exclude rows with missing values or insignificant district
-df = df.dropna()
 districts_to_exclude = [49, 65, 76]
 df = df[~df['district'].isin(districts_to_exclude)]
 
 # Define the key-value pairs for response variable and predictors by district
 district_variables = {
-    '1': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_year'),
-    '2': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'min_call_aug_sep'),
-    '3': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'min_call_aug_sep'),
-    '4': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'min_call_june_sep'),
-    '5': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_year'),
-    '6': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_year'),
-    '7': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_july_sep'),
-    '8': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_may_sep'),
-    '9': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_year'),
-    '23': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_may_sep'),
-    '48': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'min_call_june_sep'),
-    '64': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_may_sep'),
-    '80': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'apr_eddi90d'], 'avg_call_june_sep')
+    '1': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_year'),
+    '2': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'min_call_aug_sep'),
+    '3': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'min_call_aug_sep'),
+    '4': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'min_call_june_sep'),
+    '5': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_year'),
+    '6': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_year'),
+    '7': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_july_sep'),
+    '8': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_may_sep'),
+    '9': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_year'),
+    '23': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_may_sep'),
+    '48': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'min_call_june_sep'),
+    '64': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_may_sep'),
+    '80': (['apr_fx_apr_to_sep', 'may_fx_apr_to_sep', 'apr_eddi90d', 'may_swe'], 'avg_call_june_sep')
 }
 
 # Create a dictionary to store the regression results for each district
@@ -157,33 +194,48 @@ regression_results = {}
 summary_statistics = {}
 
 # Iterate over each unique district
-for district, (predictor_cols, response_col) in district_variables.items():
+for district, (predictor_col, response_col) in district_variables.items():
     # Filter the DataFrame for the current district
     district_data = df[df['district'] == int(district)]
+    
+    # Exclude rows with missing values in the predictor and response columns
+    district_data = district_data.dropna(subset=[predictor_col, response_col]) #BUG HERE REGARDING LIST FORMAT BEING UNHASHABLE
+    print(district_data)
 
     # Extract the predictors and response variables
-    predictors = district_data[predictor_cols]
+    # Handle missing values in predictor column
+    imputer = SimpleImputer()
+    predictor = imputer.fit_transform(district_data[[predictor_col]])
     response = district_data[response_col]
 
     # Perform multiple linear regression
+    X_train, X_test, y_train, y_test = train_test_split(predictor, response, test_size = 0.25, random_state=42) # Set random_state for reproducibility
+    # Reshape response variable if necessary
+    y_train = np.ravel(y_train)
+    y_test = np.ravel(y_test)   
     model = LinearRegression()
-    model.fit(predictors, response)
-    predicted_response = model.predict(predictors)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_train)
 
-    # Calculate R-squared
-    r2 = r2_score(response, predicted_response)
-
-    # Calculate RMSE
-    rmse = np.sqrt(mean_squared_error(response, predicted_response))
+    # Calculate metrics
+    r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test,y_pred)
+    #squared True returns MSE value, False returns RMSE value.
+    mse = mean_squared_error(y_test,y_pred) #default=True
+    rmse = mean_squared_error(y_test,y_pred,squared=False)
+    print("R2:", r2)
+    print("MAE:",mae)
+    print("MSE:",mse)
+    print("RMSE:",rmse)
 
     # Store regression results and summary statistics
-    regression_results[district] = (predictors, response, predicted_response)
-    summary_statistics[district] = {'R-squared': r2, 'RMSE': rmse}
+    regression_results[district] = (X_test, y_test, y_pred)
+    summary_statistics[district] = {'R-squared': r2, 'RMSE': rmse, 'MAE': mae, 'MSE': mse}
 
     # Plot predicted vs. actual response
     plt.figure(figsize=(6.5, 4.5), dpi=250)
-    sns.regplot(x=predicted_response, y=response, ci=None, color='darkblue')
-    plt.title(f'Multiple Linear Regression - District {district}', color='black')
+    sns.regplot(x=y_pred, y=y_test, ci=None, color='darkblue')
+    plt.title(f'Multiple Linear Regression Performance - District {district}', color='black')
     plt.xlabel('Predicted Response', color='gray')
     plt.ylabel('Actual Response', color='gray')
     plt.xticks(color='gray')
@@ -194,12 +246,6 @@ for district, (predictor_cols, response_col) in district_variables.items():
     plt.text(0.05, 0.85, f'RMSE: {rmse:.4f}', color='black', transform=plt.gca().transAxes)
 
     # Save the plot as a PNG file
-    filename = os.path.join(output_dir, f'district_{district}.png')
-    plt.savefig(filename, dpi=250)
+    #filename = os.path.join(output_dir, f'performance_district_{district}.png')
+    #plt.savefig(filename, dpi=250)
 
-# Display summary statistics for each district
-for district, stats in summary_statistics.items():
-    print(f"District {district}:")
-    print(f"R-squared: {stats['R-squared']:.4f}")
-    print(f"RMSE: {stats['RMSE']:.4f}")
-    print()
