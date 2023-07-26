3a136a96939f1ed4cf7dcbfc2d43a1a2d5397a5f
Emma Golub <emmaa.golub@gmail.com>
2023-07-26 15:47:22 -0700

    WDID vectors and dates updated

--- R/water_call_daily_analysis_v3_EG.R
+++ R/water_call_daily_analysis_v3_EG.R
@@ -39,23 +39,29 @@ get_call_data <- function(start_date, end_date, wdids){
   # Create a dataframe with a single column containing WDID values
   wdids_df <- data.frame(wdid = wdids)
 
-  data <- lapply(1:nrow(wdids_df), function(i) {
-    message("Pausing iteration for 1.5 minutes...")
+  tryCatch({
 
-    # add pause in loop as to not overwhelm CDSS resources, DO NOT CHANGE WHEN running large number of WDIDs/districts
-    Sys.sleep(90)
+    data <- lapply(1:nrow(wdids_df), function(i) {
+      message("Pausing iteration for 1.5 minutes...")
 
-    message("Iteration resuming...")
+      # add pause in loop as to not overwhelm CDSS resources, DO NOT CHANGE WHEN running large number of WDIDs/districts
+      Sys.sleep(90)
 
-    calls <- cdssr::get_call_analysis_wdid(
-      wdid = wdids_df$wdid[i],
-      admin_no   = "99999.00000",
-      start_date,
-      end_date,
-      api_key    = NULL
-    )
+      message("Iteration resuming...")
 
-    calls
+      calls <- cdssr::get_call_analysis_wdid(
+        wdid = wdids_df$wdid[i],
+        admin_no   = "99999.00000",
+        start_date,
+        end_date,
+        api_key    = NULL
+      )
+      calls
+
+    }, error = function(e) {
+      NULL
+    })
+
   }) %>%
     dplyr::bind_rows()
 
@@ -63,7 +69,6 @@ get_call_data <- function(start_date, end_date, wdids){
 
   return(data)
 }
-
 
 
 #################################################################################################
@@ -283,25 +288,25 @@ add_leading_zero <- function(x) {
 ################################################################################
 # Initialize variables:
 
-start_date <- "1980-01-01"
-end_date <- "1990-12-31"
+start_date <- "1970-01-01"
+end_date <- "2023-07-01"
 
 # COMMENT/UNCOMMENT FOR YOUR VECTOR OF WDIDS:
 # Rachel:
-#wdids <- c('100643','100805','100616','100897','100820','100776','100773','100722','100862','103828','100555','103574','100844','103554','103557','100532','103824','100718','100824','100759','100568','103002','103585','100684','100572','103657','100839','100843','102003','200838','202209','200809','200517') %>%
+#wdids <- c('100839','100843','102003','200740','201015','203375','301143','303347','303676','400766','400861','400868','404167','500991','502131','504081','602105','604256','700812','702108','801000','801180','902107','904288','2300679') %>%
 #  sapply(add_leading_zero)
 
 # Angus:
-#wdids <- c('200740','201015','203375','303570','303725','300853','303480','302106','300994','303712','301109','302132','300906','300530','301143','303347','303676','400577','400782','400766','400861','400868','404167','500511','501050','500621','500749','500991','502131','504081','600538','600536','602105') %>%
+#wdids <- c('2302103','2302119','4800572','4803987','4804002','4904484','4904485','4904487','6400503','6402411','6499999','6500523','6504485','7600505','7600600','8002107','8002126','8003550','0108327','0110900','0203936','0208828','0301305') %>%
 # sapply(add_leading_zero)
 
 # Emma
-wdids <- c('604256','700502','700553','700812','702108','803532','800802','800902','802227','801000','801180','900816','902262','902107','904288','2300914','2301097','2300559','2301191','2300652','2306853','2300679','2302103','2302119','6400589','6400536','6400503','6402411','6499999','8000667','8005800','8002107','8002126') %>%
+wdids <- c('0301307','0302215','0303777','0400559','0400639','0405057','0500796','0601423','0601428','0601559','0604255','0700859','0703391','0801020','0808159','0901591','0905632','2300615','2302120','2302429','4800581','4801523','4803679','4804000','6405603','6406695','8005507') %>%
   sapply(add_leading_zero)
 
 #df_call1 <- read_csv("data/daily_wdid_calls_data.csv")
 df_call1 <- read_csv("data/cdss_call_data.csv") # THIS SHOULD BE YOUR OUTPUT FROM RUNNING GET_CALL_DATA()
-df_predictor1 <- read_csv("data/annual_model_upd_eddi.csv")
+df_predictor1 <- read_csv("data/annual_model_upd_eddi_v2.csv")
 
 distr <- c(80)# 1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 64, 80)
 
@@ -316,16 +321,16 @@ df_call1$district <- trimws(df_call1$district)
 df_call <- df_call1[df_call1$district %in% c('01', '02', '03', '04', '05', '06', '07', '08', '09', '23', '64', '80'), ]
 
 
-unique_values_df_call <- df_call %>%
-  mutate(CombinedColumns = paste(analysis_wdid, district, sep = "_")) %>%
-  distinct(CombinedColumns, .keep_all = TRUE) %>%
-  select(analysis_wdid, district)
-
-unique_values_df_predictor <- df_predictor %>%
-  mutate(CombinedColumns = paste(wdid, district, sep = "_")) %>%
-  distinct(CombinedColumns, .keep_all = TRUE) %>%
-  select(wdid, district)
-unique_values_df_predictor$district <- as.character(unique_values_df_predictor$district)
+# unique_values_df_call <- df_call %>%
+#   mutate(CombinedColumns = paste(analysis_wdid, district, sep = "_")) %>%
+#   distinct(CombinedColumns, .keep_all = TRUE) %>%
+#   select(analysis_wdid, district)
+#
+# unique_values_df_predictor <- df_predictor %>%
+#   mutate(CombinedColumns = paste(wdid, district, sep = "_")) %>%
+#   distinct(CombinedColumns, .keep_all = TRUE) %>%
+#   select(wdid, district)
+# unique_values_df_predictor$district <- as.character(unique_values_df_predictor$district)
 
 #################################################################################################
 # Try out the functions:
