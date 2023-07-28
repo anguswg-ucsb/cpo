8f8bb614e06e4ae981bbeba197ab17731c80f481
Emma Golub <emmaa.golub@gmail.com>
2023-07-27 18:22:44 -0700

    function adjustments,all CDSS call data included

--- R/water_call_daily_analysis_v3_EG.R
+++ R/water_call_daily_analysis_v3_EG.R
@@ -79,7 +79,9 @@ get_call_data <- function(start_date, end_date, wdids){
 #' @param df_predictor CSV of predictor by WDID and district (i.e., "annual_model_upd_eddi.csv") {dataframe}
 #' @param distr A vector of district numbers {numeric}. Make sure to change distr and filter out df_call and df_predictor accordingly in data wrangling section.
 #' @return returns the final cleaned dataframe as "result"
-water_call_analysis <- function(df_call, df_predictor, distr){
+water_call_analysis <- function(df_call, df_predictor){
+
+  out <- list()
 
     # Detrended Call Dataset - Free River & Max Date Fix -------------------------------------------------
     call_detrended_data <- df_call %>%
@@ -113,9 +115,9 @@ water_call_analysis <- function(df_call, df_predictor, distr){
     #print(call_detrended_data)
 
     df_call$district <- as.character(df_call$district)
-    write_csv(call_detrended_data, "data/detrended_call_data.csv")
+    write_csv(call_detrended_data, "data/call_detrended_data.csv")
 
-    # call_detrended_data_by_district <- df_call %>%
+    # out$call_detrended_data_by_district <- df_call %>%
     #   #fix max priority year
     #   mutate(priority_year = year(priority_date)) %>%
     #   mutate(priroity_month = month(priority_date)) %>%
@@ -154,7 +156,11 @@ water_call_analysis <- function(df_call, df_predictor, distr){
     #df_predictor$district <- as.numeric(df_predictor$district)
     predictor_data <- predictor_data %>% mutate(district = as.numeric(district))
 
+    # replace district 5 SWE data with those from district 6, University Camp 838 for more representation
+    univ_camp_swe <- read_csv("data/district5_SWE_replacement.csv")
 
+    predictor_data$may_swe[predictor_data$district == 5 & predictor_data$year >= 1980 & predictor_data$year <= 2022] <- univ_camp_swe$may_swe
+    predictor_data$peak_swe[predictor_data$district == 5 & predictor_data$year >= 1980 & predictor_data$year <= 2022] <- univ_camp_swe$peak_swe
 
     # merge the two datasets together
     data_merge <- left_join(call_detrended_data, predictor_data, by = c("district","wdid", "year"), relationship = "many-to-many") %>%
@@ -172,6 +178,7 @@ water_call_analysis <- function(df_call, df_predictor, distr){
     write_csv(detrended_all_data_final, "data/detrended_all_data_final")
 
     # Return the final cleaned, aggregated data
+    #return(out) # FIX: need it to somehow return call_detrended data too
     return(detrended_all_data_final)
 
 }
@@ -179,13 +186,13 @@ water_call_analysis <- function(df_call, df_predictor, distr){
 #################################################################################################
 # Part 3: Linear Regression Model
 
-model <- function(detrended_all_data_final, distr){
-    rm(list = ls())
+model <- function(data, distr){
+    #rm(list = ls())
 
-    detrended_all_data_final <- detrended_all_data_final[detrended_all_data_final$district %in% distr,]
+    data <- data[data$district %in% distr,]
     # test linear regression models
-    lm1 <- lm(call_year_decimal~may_swe, detrended_all_data_final)
-    lm2 <- lm(call_year_decimal~may_fx_apr_to_sep, detrended_all_data_final)
+    lm1 <- lm(call_year_decimal~may_swe, data)
+    lm2 <- lm(call_year_decimal~may_fx_apr_to_sep, data)
     print(paste("call year vs may swe R2:", summary(lm1)$r.squared))
     print(paste("call year vs may forecast R2:", summary(lm2)$r.squared))
 }
@@ -198,6 +205,7 @@ add_leading_zero <- function(x) {
   ifelse(nchar(x) < 7, paste0("0", x), x)
 }
 
+# FOR SOME REASON THIS SECTION ONLY WORKS IF IT'S RUN AFTER THE FUNCTIONS SECTION
 ------------------------------------
 # Initializing variables for get_call_data(): --------uncomment to get call data
 
@@ -223,10 +231,10 @@ add_leading_zero <- function(x) {
 # Call datasets: ------------------------------------
 df_call_AW <- read_csv("data/cdss_call_data_AW.csv")
 df_call_EG <- read_csv("data/cdss_call_data_EG.csv")
-#df_call_RB <- read_csv("data/cdss_call_data_RB.csv")
+df_call_RB <- read_csv("data/cdss_call_data_RB.csv")
 
 # Merge
-df_call <- dplyr::bind_rows(df_call_AW, df_call_EG)#, df_call_RB)
+df_call <- dplyr::bind_rows(df_call_AW, df_call_EG, df_call_RB)
 
 # Preprocess a bit (include districts, make sure leading zeros are included)
 df_call$analysis_wdid <- add_leading_zero(df_call$analysis_wdid)
@@ -256,58 +264,49 @@ write_csv(df_call, "data/cdss_raw_daily_call_data_combined.csv")
 
 # Predictor datasets: ------------------------------------
 df_predictor <- read_csv("data/annual_model_upd_eddi_v2.csv")
-df_predictor <- df_predictor[df_predictor$district %in% c(1,2,3,4,5,6,7,8,9,23,64,80),]
+df_predictor <- df_predictor[df_predictor$district %in% c("01","02","03","04","05","06","07","08","09","23","64","80"),]
 df_predictor$wdid <- as.character(df_predictor$wdid)
-
-# unique_values_df_call <- df_call %>%
-#   mutate(CombinedColumns = paste(analysis_wdid, district, sep = "_")) %>%
-#   distinct(CombinedColumns, .keep_all = TRUE) %>%
-#   select(analysis_wdid, district)
-#
-# unique_values_df_predictor <- df_predictor %>%
-#   mutate(CombinedColumns = paste(wdid, district, sep = "_")) %>%
-#   distinct(CombinedColumns, .keep_all = TRUE) %>%
-#   select(wdid, district)
-# unique_values_df_predictor$district <- as.character(unique_values_df_predictor$district)
-
-
 
 
 #------------------------------------------------------------------------
 # Try out the functions:
 
 
-
 #get_call_data(start_date, end_date, wdids)
 
-detrended_all_data_final <- water_call_analysis(df_call, df_predictor, distr)
+result <- water_call_analysis(df_call, df_predictor)
+data_final <- result #result$detrended_all_data_final
 
 # Specify district(s) for linear regression and plotting:
-distr <- c(80)# 1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 64, 80)
+distr <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 64, 80)
 
-linear_regression <- model(detrended_all_data_final, distr)
+linear_regression <- model(data_final, distr)
 
 
 
 #################################################################################################
 # Part 5: Plotting/Visualizations
-# NEED TO FIX SO THAT CALL_DETRENDED_DATA IS ALSO AN OUTPUT OF THE FUNCTION...
 #
+# Loop for each district
+for (d in distr){
+  data <- data_final[data_final$district == d,]
+
+# # ---------------- FIX THIS COMMENTED CODE...----------------------------------
 # # Plot the Average Call Year by Year
-# p4 <- ggplot(call_detrended_data, aes_string(x="year", y = "summer_call", group = "avg_condit")) +
+# p4 <- ggplot(out$call_detrended_data, aes_string(x="year", y = "summer_call", group = "avg_condit")) +
 #   #stat_summary(fun.data=mean_cl_normal) +
 #   geom_line(aes(col = avg_condit, linetype=avg_condit)) +
 #   #geom_smooth(method='lm', formula= y~x) +
 #   theme_bw() +
-#   ggtitle(paste0("District ", paste(distr, collapse = ","), ": 6-month Average Call by Season (final clean data)"))
+#   ggtitle(paste0("District ", paste(d), ": 6-month Average Call by Season (final clean data)"))
 # ggsave(
 #   paste0("District", paste(distr, collapse = ","), "-6-monthAverageCallbySeason-finalcleandata.png"),
 #   plot = p4,
 #   width = 10, height = 10
 # )
-#
+
 # # # Plot the Average Call Year by district
-# # p5 <- ggplot(call_detrended_data_by_district, aes_string(x="year", y = "summer_call", group = "district")) +
+# # p5 <- ggplot(out$call_detrended_data_by_district, aes_string(x="year", y = "summer_call", group = "district")) +
 # #   #stat_summary(fun.data=mean_cl_normal) +
 # #   geom_line(aes(col = district, linetype=district)) +
 # #   #geom_smooth(method='lm', formula= y~x) +
@@ -325,28 +324,30 @@ linear_regression <- model(detrended_all_data_final, distr)
 # # # full grid (has repeating values)
 # # corrplot::corrplot(corr_matrix, method=c("circle"), type=c("full"), na.or.zero = TRUE)
 
-p1 <- ggplot(detrended_all_data_final, aes_string(x="year", y = "may_swe", group = "wdid")) +
+# -----------------------------------------------------------------------------------------
+
+p1 <- ggplot(data, aes_string(x="year", y = "may_swe", group = "wdid")) +
   #stat_summary(fun.data=mean_cl_normal) +
   geom_line(aes(col = as.factor(wdid), linetype = as.factor(wdid))) +
   #geom_smooth(method='lm', formula= y~x) +
   theme_bw() +
   ggtitle("May SWE by WDID")
 
-p2 <- ggplot(detrended_all_data_final, aes_string(x="year", y = "may_fx_apr_to_sep", group = "wdid")) +
+p2 <- ggplot(data, aes_string(x="year", y = "may_fx_apr_to_sep", group = "wdid")) +
   #stat_summary(fun.data=mean_cl_normal) +
   geom_line(aes(col = as.factor(wdid), linetype = as.factor(wdid))) +
   #geom_smooth(method='lm', formula= y~x) +
   theme_bw() +
   ggtitle("May NRCS Forecast by WDID")
 
-p3 <- ggplot(detrended_all_data_final, aes_string(x="year", y = "call_year_decimal", group = "wdid")) +
+p3 <- ggplot(data, aes_string(x="year", y = "call_year_decimal", group = "wdid")) +
   #stat_summary(fun.data=mean_cl_normal) +
   geom_line(aes(col = as.factor(wdid), linetype = as.factor(wdid))) +
   #geom_smooth(method='lm', formula= y~x) +
   theme_bw() +
   ggtitle("Revised Summer Call Year by WDID")
 
-plot_title <- paste0("District ", paste(distr, collapse = ","), " - Regression Summary - Clean Data")
+plot_title <- paste0("District ", paste(d), " - Regression Summary - Clean Data")
 ggsave(
   paste0(plot_title, ".png"),
   width = 14, height = 8,
@@ -356,21 +357,21 @@ ggsave(
 )
 
 # Plot the May SWE Vs Summer Average Call
-p6 <- ggplot(detrended_all_data_final, aes(x = may_swe, y = call_year_decimal)) +
+p6 <- ggplot(data, aes(x = may_swe, y = call_year_decimal)) +
   stat_summary(fun.data=mean_cl_normal) +
   geom_point(aes(color = as.factor(wdid))) +
   geom_smooth(method='lm', formula= y~x) +
   theme_bw() +
   ggtitle("Summer Average Call Year vs May SWE by WDID")
 
-p7 <- ggplot(detrended_all_data_final, aes(x = may_fx_apr_to_sep, y = call_year_decimal)) +
+p7 <- ggplot(data, aes(x = may_fx_apr_to_sep, y = call_year_decimal)) +
   stat_summary(fun.data=mean_cl_normal) +
   geom_point(aes(color = as.factor(wdid))) +
   geom_smooth(method='lm', formula= y~x) +
   theme_bw() +
   ggtitle("Summer Average Call vs May NRCS Forecast by WDID")
 
-plot_title <- paste0("District ", paste(distr, collapse = ","), " - Regression Analysis Summary")
+plot_title <- paste0("District ", paste(d), " - Regression Analysis Summary")
 ggsave(
   paste0(plot_title, ".png"),
   width = 10, height = 10,
@@ -378,3 +379,5 @@ ggsave(
                top = plot_title,
                right = "")
 )
+
+}
